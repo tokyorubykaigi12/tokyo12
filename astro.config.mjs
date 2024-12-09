79986a505c1fc6e43c5bd86992b44aa280b48b0b
terfno <terfno@cookpad.com>
2024-12-09 16:08:34 +0900

    :package: add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import preact from '@astrojs/preact';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://regional.rubykaigi.org",
   base: "/tokyo12",
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
 });
